cc3fb56a14ede80f6ec08b2d4618a31b1ff24ea0
Paul Acklam <paulacklam@gmail.com>
2016-11-10 12:26:27 +0000

    Bare bones functioning version of the parser

--- test_PA.py
+++ test_PA.py
@@ -1,16 +1,47 @@
-import xml.etree.ElementTree as ET
+import xml.etree.ElementTree as etree
 import os
+import csv
+import sys
 
-mydir = os.getcwd() + "/LRG_public_xml_files/"
+lrg = sys.argv[1]
 
-tree = ET.parse("../LRG_Data/LRG_1.xml")
+filepath = '../LRG_Data/' + lrg + '.xml'
 
-root = tree.getroot()	
+if os.path.isfile(filepath) != True:
+	print("Please specify a valid LRG number")
+else:
+	# run the function
 
-print(root[0][0].text)
-print(root[0][1].text)
-print(root[0][2].text)
-print(root[0][3].text)
-print(root[0][4][0].text)
-print(root[0][4][1].text)
-print(root[0][4][2][0].text)
+
+# root = etree.parse(filepath)
+
+# gDNAseq = root.find('./fixed_annotation/sequence').text
+
+# mylist = []
+
+# for transcript in root.findall('./fixed_annotation/transcript'):
+# 	tx = transcript.attrib['name']
+# 	for exon in transcript.findall('./exon'):
+# 		exonnumber = exon.attrib['label']
+# 		coordinates = exon.find('coordinates')
+# 		coord_sys = coordinates.attrib['coord_system']
+# 		start = coordinates.attrib['start']
+# 		end = coordinates.attrib['end']
+# 		seq = gDNAseq[int(start)-1:int(end)]
+# 		tmp = [tx, exonnumber, coord_sys, start, end, seq]
+# 		mylist.append(tmp)
+
+# for row in mylist:
+# 	print(row)
+
+# headers = ['Transcript_ID', 'Exon_no', 'Coordinate_system', 'Start', 'End', 'Sequence']
+
+# mylist.insert(0, headers)
+
+# myoutput = os.getcwd() + "/output.csv"
+
+# with open(myoutput, 'wb') as f:
+# 	writer = csv.writer(f)
+# 	writer.writerows(mylist)
+
+# print("Done")
