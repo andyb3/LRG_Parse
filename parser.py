1bc11f617c5d6bda39cdec1aae8eed95231912b2
Andy Bond <andybond87@gmail.com>
2016-12-13 17:21:03 +0000

    Added additional error checking steps

--- parser.py
+++ parser.py
@@ -34,23 +34,21 @@ def checkValidLRG(userLRG):
 
 def readLRG(lrg):
 
-    '''Takes the LRG code and fetches the relevant XML file from the EBI ftp server'''
+    '''Takes the LRG code and requests the relevant XML file from the EBI FTP server'''
 
     lrgURL = 'http://ftp.ebi.ac.uk/pub/databases/lrgex/' + lrg + '.xml'
     try:
-        tree = etree.parse(urllib2.urlopen(lrgURL))
-    #Catch any HTTP errors and exit program with error message if site can't be reached.
-    except urllib2.HTTPError, e:
-        print("Unable to retrieve LRG file from web")
-        return #return a Null value so that program does not continue
+        lrgXML = urllib2.urlopen(lrgURL) #Request LRG file
+        assert (lrgXML.getcode() == 200), "Unable to retrieve LRG file from web" # Check HTTP response = 200 (indicates request was successful)
+    #Catch any HTTP or URL errors and exit program with error message if site can't be reached.
+    except (urllib2.HTTPError, urllib2.URLError):
+        raise ValueError("Unable to retrieve LRG file from web")
     else:
         #Check that the file contains the lrg tag and conforms to version 1.9 standards.
         #An error message is displayed and program exits if these conditions are not met.
-        if tree.getroot().tag == 'lrg' and tree.getroot().attrib['schema_version'] == "1.9":
-            return tree
-        else:
-            print("File does not conform to lrg version 1.9 specification.")
-            return #return a Null value so that program does not continue
+        tree = etree.parse(lrgXML)
+        assert (tree.getroot().tag == 'lrg' and tree.getroot().attrib['schema_version'] == "1.9"), "File does not conform to LRG version 1.9 specification."
+        return tree
 
 def getGeneLevData(tree):
 
@@ -79,25 +77,27 @@ def getGeneLevData(tree):
     # return geneData dictionary
     return geneData
 
-def getSeq(tree):
+def getGenomicSeq(tree):
+    
     '''
-    Maps LRG sequence to main assembly sequence.
+    Builds genomic (main assembly) sequence from LRG sequence
     Accounts for all differences (mismatch, insertions and deletions) between LRG and main assembly sequence.
-    Returns main assembly DNA sequence and lookup dictionary to convert LRG sequence positions to main assembly sequence positions. 
+    Returns genomic sequence and lookup dictionary to convert LRG sequence positions to main assembly sequence positions. 
     '''
+    
     #Get complete LRG sequence and create copy that will be converted to main assembly sequence 
     lrgSeqFull = tree.find('./fixed_annotation/sequence').text
     mainAssemSeq = lrgSeqFull
     #Get end position of LRG sequence and check LRG sequence is expected length
     mainAssemData = tree.find("updatable_annotation/annotation_set[@type='lrg']/mapping[@type='main_assembly']/mapping_span")
     lrgEnd = int(mainAssemData.attrib['lrg_end'])
-    assert (len(lrgSeqFull) == lrgEnd), "Discrepency between stated length of LRG sequennce and sequence provided"
+    assert (len(lrgSeqFull) == lrgEnd), "Discrepency between stated length of LRG sequence and sequence provided"
     #Initialise lookup dictionary  with the LRG positions
     convertPosDict = {}
     for i in range(lrgEnd):
         convertPosDict[i+1] = i+1
     #Loop through differences between LRG and main assembly
-    #Modify mainAssemSeq and converPosDict to account for each difference.
+    #On each iteration, modify mainAssemSeq and convertPosDict to account for each difference.
     for diff in mainAssemData.findall("diff"):
         #Convert LRG start and end positions of the variant to the current main assembly start and end positions
         startPos = convertPosDict[int(diff.attrib['lrg_start'])]
@@ -107,7 +107,7 @@ def getSeq(tree):
         otherSeq = diff.attrib['other_sequence']
         #If it's a mismatch...
         if diff.attrib['type'] == 'mismatch':
-            #Check the stated LRG sequence matches the actual sequence at that position
+            #Check the stated LRG sequence matches the current sequence at that position
             assert (mainAssemSeq[startPos-1:endPos] == lrgSeq), "Error when converting to genomic sequence. LRG sequence not as expected."
             #Add the change into the main assembly sequence
             mainAssemSeq = mainAssemSeq[:startPos-1] + otherSeq + mainAssemSeq[endPos:]
@@ -123,15 +123,15 @@ def getSeq(tree):
                 convertPosDict[i] += insLength 
         #If it's an insertion into the LRG sequence (effectively a deletion in main assembly)...
         elif diff.attrib['type'] == 'lrg_ins':
-            #Check the stated LRG sequence matches the actual sequence at that position            
+            #Check the stated LRG sequence matches the current sequence at that position            
             assert (mainAssemSeq[startPos-1:endPos] == lrgSeq), "Error when converting to genomic sequence. LRG sequence not as expected."
             #Check there is no main assembly sequence 
             assert (otherSeq == "-"), "Error when converting to genomic sequence. LRG sequence not as expected."
             #Remove the deleted sequence from the main assembly sequence    
             mainAssemSeq = mainAssemSeq[:startPos-1] + mainAssemSeq[endPos:]
-            delLength = len(lrgSeq)
             #For LRG positions that lie within deleted region, 
             #update the lookup dictionary so they map to the first base following the deletion in main assembly
+            delLength = len(lrgSeq)
             posConvDel = endPos - (delLength-1)
             for i in range(startPos, endPos+1):
                 convertPosDict[i] = posConvDel
@@ -159,9 +159,9 @@ def getExons(tree, geneData, mainAssemSeq, convertPosDict):
             coordinates = exon.find('coordinates')
             coord_sys = coordinates.attrib['coord_system'] # LRG number
             start = coordinates.attrib['start'] # LRG start position
-            start = convertPosDict[int(start)]
+            start = convertPosDict[int(start)]# Start position in main assembly sequence
             end = coordinates.attrib['end'] # LRG End position
-            end = convertPosDict[int(end)]
+            end = convertPosDict[int(end)]# End position in main assembly sequence
             # Convert LRG coordinates to genomic coordinates
             # If the gene is located on the forward strand...
             if geneData['strand'] == "1":
@@ -171,6 +171,8 @@ def getExons(tree, geneData, mainAssemSeq, convertPosDict):
             elif geneData['strand'] == "-1":
                 gDNA_start = (int(geneData['other_end']) - int(start)) + 1
                 gDNA_end = (int(geneData['other_end']) - int(end)) + 1
+            else:
+                raise ValueError("Strand orientation not specified. Unable to calculate genomic coordinates.")
             # Use the LRG coordinates to take a slice the DNA and retrieve exon sequence
             seq = mainAssemSeq[int(start)-1:int(end)]
             # Create list containing all details to be included in CSV row for that exon
@@ -193,26 +195,27 @@ def writeCSV(headers, exonList, LRG):
 
 ###############################################################################
 
-# Check that the user has provided the correct number of arguments
-assert (len(sys.argv) == 3), "Incorrect number of arguments"
-
-# Check that the user has provided one of the two correct flags
-assert (sys.argv[1] in ['-g', '-l']), "Wrong flag"
-
-# Depending on whether the user has used the '-g' or '-l' flag...
-if sys.argv[1] == '-g':
-    LRG = checkGeneDict(sys.argv[2])
-elif sys.argv[1] == '-l':
-    LRG = checkValidLRG(sys.argv[2])
-
-tree = readLRG(LRG)
-if tree:
-    mainAssemSeq, convertPosDict = getSeq(tree)
+if __name__ == '__main__':
+    # Check that the user has provided the correct number of arguments
+    assert (len(sys.argv) == 3), "Incorrect number of arguments. Please see README file for usage instructions."
+    
+    # Check that the user has provided one of the two correct flags
+    assert (sys.argv[1] in ['-g', '-l']), "Unknown flag used. Please see README file for usage instructions."
+    
+    # Depending on whether the user has used the '-g' or '-l' flag...
+    if sys.argv[1] == '-g':
+        LRG = checkGeneDict(sys.argv[2]) #Returns LRG number for user specified gene symbol.
+    elif sys.argv[1] == '-l':
+        LRG = checkValidLRG(sys.argv[2]) #Checks user specified LRG is valid
+    
+    tree = readLRG(LRG)
+    
+    mainAssemSeq, convertPosDict = getGenomicSeq(tree)
     geneData = getGeneLevData(tree)
     exonList = getExons(tree, geneData, mainAssemSeq, convertPosDict)
     headers = ['LRG_Number', 'Build', 'HGNCID', 'Gene', 'Transcript_ID', 'Exon_no', 'Chrom', 'Start', 'End', 'Sequence']
     writeCSV(headers, exonList, LRG)
     #for row in exonList:
     #    print(row)
-
-#print("Done")
+    
+    #print("Done")
